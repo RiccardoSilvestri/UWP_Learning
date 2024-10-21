15f79959e9fe081bc03e1d5d9d8ae9c0b89967dd
RiccardoSilvestri <r.silvestri04@gmail.com>
2024-10-21 17:03:09 +0200

    Add project files.

---
+++ UserRegistry/Views/ChiamateHTTP.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using UserRegistry.Models;
+
+
+namespace UserRegistry.Views
+{
+    public sealed partial class ChiamateHTTP : Page
+    {
+        HttpClient httpCLient = new HttpClient();
+        public ChiamateHTTP()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void BtnHttpGetComments_Click(object sender, RoutedEventArgs e)
+        {
+            var comments = await httpCLient.GetFromJsonAsync<List<Comment>>("https://jsonplaceholder.typicode.com/comments");
+            CommentListView.ItemsSource = comments;
+        }
+    }
+}
